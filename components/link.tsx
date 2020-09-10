df28457f8598019b5daea24ee836acaecf25df8e
Colin Ramsay <github@happyandlost.co.uk>
2020-09-10 15:23:03 +0100

    fix: remove comments

--- components/link.tsx
+++ components/link.tsx
@@ -12,22 +12,6 @@ interface Props {
 const Link = ({ href, link, className, children }: Props) => {
   const router = useRouter()
   const isCurrentPage = router.pathname === link
-  // const onClick = (event) => {
-  //   // Do nothing if it's an external link
-  //   if (link.startsWith("http")) return;
-
-  //   event.preventDefault();
-  //   // Set the router to the new url.
-  //   actions.router.set(link);
-
-  //   // Scroll the page to the top
-  //   window.scrollTo(0, 0);
-
-  //   // if the menu modal is open, close it so it doesn't block rendering
-  //   if (state.theme.isMobileMenuOpen) {
-  //     actions.theme.closeMobileMenu();
-  //   }
-  // };
 
   return (
     <NextLink href={href ? href : link} as={href ? link : null}>
